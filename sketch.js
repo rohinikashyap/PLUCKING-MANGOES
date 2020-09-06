1347908d461dc49b9f006cee434a1ab4b2616f2c
rohinikashyap <69142081+rohinikashyap@users.noreply.github.com>
2020-09-06 14:33:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var tree, stone,ground, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7;
+var boy,boyImg;
+
+function preload()
+{
+	boyImg = loadImage("boy.png");
+}
+
+function setup() {
+	createCanvas(1350, 600);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	boy = createSprite(200,500);
+	boy.addImage(boyImg);
+	boy.scale = 0.1;
+	tree = new Tree(900,350,30,300);
+	ground = new Ground(600,600,2000,20);
+
+	mango1 = new Mango(900,250,18);
+	mango2 = new Mango(800,200,18);
+	mango3 = new Mango(800,280,18);
+	mango4 = new Mango(1000,250,18);
+	mango5 = new Mango(670,300,18);
+	mango6 = new Mango(950,200,18);
+	mango7 = new Mango(1100,300,18);
+
+	stone = new Stone(150,550,15);
+	boyShot = new Shot(stone.body,{x:150,y:452});
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  
+  Engine.update(engine);
+
+  background("lightgray");
+  tree.display();
+  ground.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  mango7.display();
+
+  stone.display();
+  boyShot.display();
+  detectCollision(stone,mango1);
+  detectCollision(stone,mango2);
+  detectCollision(stone,mango3);
+  detectCollision(stone,mango4);
+  detectCollision(stone,mango5);
+  detectCollision(stone,mango6);
+  detectCollision(stone,mango7);
+  drawSprites();
+ 
+}
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    boyShot.fly();
+}
+function detectCollision(lstone,lmango){
+	mangoBodyPosition=lmango.body.position
+	stoneBodyPosition=lstone.body.position
+	var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
+	if(distance<=lmango.r+lstone.r){
+		Matter.Body.setStatic(lmango.body,false);
+	}
+}
+
+function keyPressed(){
+
+	if(keyCode === 32){
+		Matter.Body.setPosition(stone.body,{x:150,y:550})
+		boyShot.attach(stone.body);
+	}
+}
